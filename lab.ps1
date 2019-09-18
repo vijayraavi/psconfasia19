7ee37cc3d05477ee5052e56aa4198dc64c981c3b
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-09-18 18:00:47 +0530

    Restructured talk

---
+++ lab.ps1
@@ -0,0 +1,8 @@
+﻿$labName = 'psconfasia19'
+New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV
+
+Add-LabMachineDefinition -Name mach1 -Roles FileServer -Memory 6gb -OperatingSystem 'Windows Server 2016 Datacenter (Desktop Experience)'
+
+Install-Lab
+
+Remove-Lab -Confirm:$false
